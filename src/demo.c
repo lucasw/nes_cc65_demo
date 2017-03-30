a010e1e246a3b9f86715ddfbcbd5fd2b0a63829d
Lucas Walter <wsacul@gmail.com>
2017-03-30 06:35:57 -0700

    Enemies which advance down the screen, but no collision detection yet

--- src/demo.c
+++ src/demo.c
@@ -11,15 +11,21 @@ static unsigned char touch;
 static unsigned char frame;
 
 // two players coords
-static unsigned char cat_x[2];
-static unsigned char cat_y[2];
+static unsigned char player_x[2];
+static unsigned char player_y[2];
 
 #define NUM_BULLETS 4
 static unsigned char bx[2][NUM_BULLETS];
 static unsigned char by[2][NUM_BULLETS];
 static unsigned char b_ind[2];
 
-// first player metasprite, data structure explained in neslib.h
+// enemies
+#define NUM_ENEMIES 4
+static unsigned char enemy_x[NUM_ENEMIES];
+static unsigned char enemy_y[NUM_ENEMIES];
+// random number for this enemy
+static unsigned char enemy_r[NUM_ENEMIES];
+
 // x offset, y offset, tile, attribute
 /*
 76543210
@@ -30,7 +36,16 @@ static unsigned char b_ind[2];
 |+------- Flip sprite horizontally
 +-------- Flip sprite vertically
 */
-const unsigned char meta_cat1[]={
+
+const unsigned char enemy_meta[] = {
+  8,  0,  0x52,  3,
+  16,  0,  0x52,  3 | (1 << 6),
+  128   // TODO(lucasw) what is this?
+};
+
+
+// first player metasprite
+const unsigned char player_meta1[] = {
   0,  0,  0x50,  0,  // upper left corner ear
   8,  0,  0x51,  0,
   16,  0,  0x50,  1 << 6,
@@ -44,7 +59,7 @@ const unsigned char meta_cat1[]={
 };
 
 // second player metasprite, the only difference is palette number
-const unsigned char meta_cat2[]={
+const unsigned char player_meta2[] = {
   0,  0,  0x50,  1,
   8,  0,  0x51,  1,
   16,  0,  0x50,  1 | (1 << 6),
@@ -57,18 +72,32 @@ const unsigned char meta_cat2[]={
   128
 };
 
+// http://www.arklyffe.com/main/2010/08/29/xorshift-pseudorandom-number-generator/
+unsigned char __fastcall__ xorshift8(unsigned char y8)
+{
+  y8 ^= (y8 << 7);
+  y8 ^= (y8 >> 5);
+  return y8 ^= (y8 << 3);
+}
+
 void main(void)
 {
   // enable rendering
   ppu_on_all();
 
   // set initial coords
-  cat_x[0] = 52;
-  cat_y[0] = 100;
-  cat_x[1] = 180;
-  cat_y[1] = 100;
+  player_x[0] = 52;
+  player_y[0] = 200;
+  player_x[1] = 180;
+  player_y[1] = 200;
 
-  // init other vars
+  for (i = 0; i < NUM_ENEMIES; ++i)
+  {
+    enemy_x[i] = 16 + i << 5;
+    enemy_y[i] = 32;
+    enemy_r[i] = 30 + i;
+  }
+
   for (i = 0; i < 2; ++i)
   {
     b_ind[i] = 0;
@@ -106,16 +135,25 @@ void main(void)
     pal_col(26, 0x3A);  // set second sprite color 2
     pal_col(27, 0x30);  // set second sprite color 3
 
+    pal_col(28, 0x13);  // set second sprite color 1
+    pal_col(29, 0x23);  // set second sprite color 2
+    pal_col(30, 0x33);  // set second sprite color 3
+
     // process players
     spr = 0;
 
     for (i = 0; i < 2; ++i)
     {
       // display metasprite
-      spr = oam_meta_spr(cat_x[i], cat_y[i], spr, i ? meta_cat2 : meta_cat1);
+      spr = oam_meta_spr(player_x[i], player_y[i], spr, i ? player_meta2 : player_meta1);
     }
 
-    // cannon blasts
+    for (i = 0; i < NUM_ENEMIES; ++i)
+    {
+      spr = oam_meta_spr(enemy_x[i], enemy_y[i], spr, enemy_meta);
+    }
+
+    // player cannon blasts
     // set sprite in OAM buffer, chrnum is tile, attr is attribute, sprid is offset in OAM in bytes
     // returns sprid+4, which is offset for a next sprite
     // oam_spr(x, y, chrnum, attr, sprid);
@@ -128,6 +166,7 @@ void main(void)
       }
     }
 
+    ///////////////////////////////////////////////////////////////////////////
     // non graphics stuff
 
     for (i = 0; i < 2; ++i)
@@ -136,34 +175,47 @@ void main(void)
       pad = pad_trigger(i);
       if (pad & PAD_A)
       {
-        cat_y[i] += 2;
-        bx[i][b_ind[i]] = cat_x[i];
-        by[i][b_ind[i]] = cat_y[i];
+        player_y[i] += 2;
+        bx[i][b_ind[i]] = player_x[i];
+        by[i][b_ind[i]] = player_y[i];
         ++b_ind[i];
         b_ind[i] %= NUM_BULLETS;
       }
 
       // poll pad and change coordinates
       pad = pad_state(i);
-      if (pad & PAD_LEFT && cat_x[i] > 0) cat_x[i] -= 2;
-      if (pad & PAD_RIGHT && cat_x[i] < 232) cat_x[i] += 2;
-      if (pad & PAD_UP && cat_y[i] > 0) cat_y[i] -= 2;
-      if (pad & PAD_DOWN && cat_y[i] < 212) cat_y[i] += 2;
+      if (pad & PAD_LEFT && player_x[i] > 0) player_x[i] -= 2;
+      if (pad & PAD_RIGHT && player_x[i] < 232) player_x[i] += 2;
+      if (pad & PAD_UP && player_y[i] > 0) player_y[i] -= 2;
+      if (pad & PAD_DOWN && player_y[i] < 212) player_y[i] += 2;
     }
 
     // check for collision for a smaller bounding box
     // metasprite is 24x24, collision box is 20x20
 
-    if (!(cat_x[0] + 22 < cat_x[1] + 2 ||
-        cat_x[0] + 2 >= cat_x[1] + 22 ||
-        cat_y[0] + 22 < cat_y[1] + 2 ||
-        cat_y[0] + 2 >= cat_y[1] + 22))
+    if (!(player_x[0] + 22 < player_x[1] + 2 ||
+        player_x[0] + 2 >= player_x[1] + 22 ||
+        player_y[0] + 22 < player_y[1] + 2 ||
+        player_y[0] + 2 >= player_y[1] + 22))
     {
       touch = 1;
     }
     else
     {
       touch = 0;
+    }
+
+    for (i = 0; i < NUM_ENEMIES; ++i)
+    {
+      enemy_r[i] = xorshift8(enemy_r[i]);
+      if (frame % 4 == 0)
+      {
+        enemy_y[i] += 1;
+        if (enemy_r[i] > 128)
+          enemy_x[i] += enemy_r[i] >> 7;
+        else
+          enemy_x[i] -= enemy_r[i] >> 7;
+      }
     }
 
     // bullets
